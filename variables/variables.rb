93ce2e70c13bbf7a853f3864d4a71612de384356
brayoh <bkuriakb@gmail.com>
2017-09-05 18:07:12 +0300

    Declare variables and print single line statements to the console

---
+++ variables/variables.rb
@@ -0,0 +1,8 @@
+# Declaring variables in ruby
+car = "VolksWagen"
+food = "Chicken"
+person = "Tom"
+home = "Mombasa"
+
+puts "#{person} is from #{home} and drives a #{car}."
+puts "His favorite meal is #{food}"
